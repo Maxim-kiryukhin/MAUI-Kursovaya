f8449ea54fe04c333554d2cf0065811fccaf9188
Maxim-kiryukhin <147792917+Maxim-kiryukhin@users.noreply.github.com>
2026-05-24 23:23:35 +0500

    first commit

---
+++ Всё ок, только Ip ставить/wwwroot/components/Orders/OrderItem.jsx
@@ -0,0 +1,37 @@
+import "./OrderItem.css";
+
+function OrderItem(props) {
+    return (
+        <>
+            <div className="order-item">
+                <div
+                    className="pic"
+                    onClick={() => props.GoodPage("Good", props.good)}
+                >
+                    <img
+                        src={`${import.meta.env.VITE_API_URL}/${props.good.pic}`}
+                        alt=""
+                    />
+                </div>
+                <div className="desc">
+                    <div className="name">
+                        <p className="montserrat">{`${props.good.name}`}</p>
+                    </div>
+                    <div className="price">
+                        <p className="montserrat">{`${props.good.price}`} р</p>
+                    </div>
+                </div>
+                <div className="buttons">
+                    <div className="amount">
+                        <p className="montserrat">{props.amount}</p>
+                    </div>
+                </div>
+                <div className="final-price">
+                    <p className="montserrat">{`${props.price} р`}</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default OrderItem;
---
+++ Перенесенное, только Ip ставить/Front/src/data/BigFooter.js
@@ -0,0 +1,8 @@
+// Картинки для компонента BigFooter
+export const getBigFooter = [
+    {
+        pic1: "/assets/Icons/social/max.svg",
+        pic2: "/assets/Icons/social/odnokl.svg",
+        pic3: "/assets/Icons/social/vk.png",
+    },
+];
