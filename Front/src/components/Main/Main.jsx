505b2b5c413e1a707d2c4cc7f9a51fb55b31a31b
Maxim-kiryukhin <147792917+Maxim-kiryukhin@users.noreply.github.com>
2026-06-01 15:34:34 +0500

    dodelal vsyo^_^

---
+++ Front/src/components/Main/Main.jsx
@@ -0,0 +1,122 @@
+import "../Main/Main.css";
+import "../Halfs/Halfs";
+import Halfs from "../Halfs/Halfs";
+import "../Footer/Footer";
+import Footer from "../Footer/Footer";
+import { getHalfs } from "../../data/Halfs";
+import { useState } from "react";
+import { useEffect } from "react";
+
+function Main(props) {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+    return (
+        <>
+            <div className="main">
+                <div className="main_header">
+                    {/* <div className='header_left'>
+                        Lorem, ipsum dolor.
+                    </div> */}
+                    <div className="main_header_middle">
+                        <p className="martian-mono">Вкус&Традиция</p>
+                    </div>
+                    {/* <div className='main_header_right'>
+                        Lorem, ipsum dolor.
+                    </div> */}
+                </div>
+                <div className="text">
+                    <p className="oswald">Выберите интересующий ресторан</p>
+                </div>
+                <div className="text_small">
+                    <p className="comfortaa">
+                        Быстрый ланч, уютный вечер вдвоем, шумные посиделки с
+                        компанией. В нашем меню есть всё, чтобы сделать ваш
+                        момент особенным.
+                    </p>
+                </div>
+            </div>
+
+            {windowWidth > 600 ? (
+                <>
+                    <Halfs
+                        order="0"
+                        OnClick={props.OnClick}
+                        {...getHalfs[0]}
+                    ></Halfs>
+                    <Halfs
+                        order="1"
+                        OnClick={props.OnClick}
+                        {...getHalfs[1]}
+                    ></Halfs>
+                    <Halfs
+                        order="0"
+                        OnClick={props.OnClick}
+                        {...getHalfs[2]}
+                    ></Halfs>
+                    <Halfs
+                        order="1"
+                        OnClick={props.OnClick}
+                        {...getHalfs[3]}
+                    ></Halfs>
+                </>
+            ) : (
+                <>
+                    <Halfs
+                        order="0"
+                        OnClick={props.OnClick}
+                        {...getHalfs[0]}
+                    ></Halfs>
+                    <Halfs
+                        order="0"
+                        OnClick={props.OnClick}
+                        {...getHalfs[1]}
+                    ></Halfs>
+                    <Halfs
+                        order="0"
+                        OnClick={props.OnClick}
+                        {...getHalfs[2]}
+                    ></Halfs>
+                    <Halfs
+                        order="0"
+                        OnClick={props.OnClick}
+                        {...getHalfs[3]}
+                    ></Halfs>
+                </>
+            )}
+
+            <div className="main-about_half">
+                <div className="first_half">
+                    <div className="pic">
+                        <img src="/assets\Icons\site.svg" alt="" />
+                    </div>
+                    <div className="first_half_desc_text">
+                        <h1 className="martian-mono">Вкус&Традиция</h1>
+                    </div>
+                    <button
+                        className="first_half_desc_button"
+                        onClick={() => props.OnClick("About")}
+                    >
+                        <p className="montserrat">О нас</p>
+                    </button>
+                </div>
+                <img
+                    className="second_half"
+                    src="/assets/Restoraunt_Photos/10724.jpg"
+                    alt=""
+                />
+            </div>
+
+            <Footer></Footer>
+        </>
+    );
+}
+
+export default Main;
